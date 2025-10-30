5aac0a9874fd1c683350a455a3114924fec65cc3
eduuHT <eduuu.ht@gmail.com>
2025-10-29 18:34:50 -0700

    Add simple calculator

---
+++ MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using PropertyChanged;
+
+namespace CalculatorNotifyChanged
+{
+    [AddINotifyPropertyChangedInterface]
+    internal class MainPageViewModel
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+
+        public int Result { get; set; }
+
+        public ICommand SumarCommand =>
+            new Command(() =>
+            {
+                Result = Num1 + Num2;
+            });
+    }
+}
